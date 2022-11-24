e393946c643df6e0b7145da2c4bc6af06603fec3
TonyWu <75903587+TonyWu2333@users.noreply.github.com>
2022-11-25 00:18:08 +0800

    Add files via upload

---
+++ 随机出题-判断 .cpp
@@ -0,0 +1,209 @@
+//著作人：吴天卓
+//github主页：https://github.com/TonyWu2333
+//个人博客：www.t-on-y-best.xyz
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define MAX 999999
+
+char temp[666];
+
+string line[10000];
+
+int collect_p[501];
+
+void save_collect(){
+	
+	ofstream outfile;
+	
+	outfile.open("./data/collect2");
+	
+	for(int i = 0; i <= 500; i++){
+		
+		outfile << collect_p[i] << endl;
+		
+	}
+	
+	outfile.close();
+	
+}
+
+int main()
+{
+	
+	ifstream fp;
+	
+	fp.open("readme.txt");
+	
+	int i = 1;
+	
+	while(fp.good()){
+		
+		fp.getline(temp,666);
+		
+		cout << temp << endl;
+		
+	}
+	
+	if(fp.eof()){
+		
+	}else if(fp.fail()){
+	
+		cout << "data mismatch";
+	
+	}else{
+		
+		cout << "other reason";
+		
+	}cout << endl;
+	
+	fp.close();
+	
+	fp.open("./data/problems2");
+	
+	i = 1;
+	
+	while(fp.good()){
+		
+		fp.getline(temp,666);
+		
+		line[i++] = temp;
+		
+	}
+	
+	if(fp.eof()){
+		
+		cout << "题库读取成功";
+		
+	}else if(fp.fail()){
+	
+		cout << "data mismatch";
+	
+	}else{
+		
+		cout << "other reason";
+		
+	}cout << endl << endl;
+	
+	fp.close();
+	
+	fp.open("./data/collect2");
+	
+	int j = 0;
+	
+	while(fp.good()){
+		
+		fp >> collect_p[j++];
+		
+	}
+	
+	if(fp.eof()){
+		
+		cout << "收藏题库读取成功" ;
+		
+	}else if(fp.fail()){
+	
+		cout << "data mismatch";
+	
+	}else{
+		
+		cout << "other reason";
+		
+	}cout << endl << endl;
+	
+	fp.close();
+	
+	srand(time(0));
+	
+	int problem_num = i / 7;
+	
+	int ran;
+	
+	string ans;
+	
+	while(1){
+		
+		ran = rand() % problem_num;
+		
+		cout << line[ran*3+1] << endl;
+		
+		cout << "你的答案：";
+		
+		cin >> ans;
+		
+		if(ans == "exit") break; 
+		
+		cout << line[ran*3+2] << endl << endl;
+		
+		if(ans == "collect"){
+			
+			collect_p[ran] = 1;
+			
+			save_collect();
+			
+			cout << "已收藏此题" << endl << endl;
+			
+		}
+		
+		if(ans == "reset"){
+			
+			for(int i = 0; i <= 500; i++) collect_p[i] = 0;
+			
+			save_collect();
+			
+			cout << "已重置" << endl << endl;
+			
+		}
+		
+		if(ans == "exam"){
+			
+			cout << "考试模式开始" << endl << endl;
+			
+			for(int i = 0; i <= 500; i++){
+				
+				if(collect_p[i] == 1){
+					
+					ran = i; 
+					
+					cout << line[ran*3+1] <<endl << endl;
+					
+					cout << "你的答案：";
+		
+					cin >> ans;
+		
+					if(ans == "exit") break; 
+		
+					cout << line[ran*3+2] << endl << endl;
+					
+					if(ans == "pass"){
+						
+						collect_p[ran] = 0;
+						
+						save_collect();
+						
+					} 
+					
+				}
+				
+			}
+			
+			cout << "考试结束" << endl << endl;
+			
+		}
+		
+	}
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
